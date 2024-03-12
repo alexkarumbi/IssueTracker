ac2687bf8f9cb2ff9ad8bf7aad3c106b5410b67b
alexkarumbi <alexkarumbimuhia@gmail.com>
2024-03-12 13:44:46 +0300

    Set up file structure and api,db,ui

---
+++ app/issues/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from '@radix-ui/themes'
+
+const IssuesPage = () => {
+  return (
+    <>
+    <h2>Issues Page</h2>
+    <Button>New Issue</Button>
+    </>
+  )
+}
+
+export default IssuesPage
